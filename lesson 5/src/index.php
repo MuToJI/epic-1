eeef1ce632cf576ab247d8fea70fad6e1378f55e
Anton V. Nazarov <a.nazarov@advmaker.net>
2016-02-02 15:37:19 +0300

    ordering, template

--- lesson 5/src/index.php
+++ lesson 5/src/index.php
@@ -20,7 +20,7 @@ if (!empty($_POST['command']) && $_POST['command'] === 'save' && valid_request($
     $statement->execute([':message' => $_POST['message'], ':user_id' => $user['id'], ':time' => date('Y-m-d H:i:s')]);
 }
 
-$messages = $mysql->query('SELECT m.`message`, m.`time`, u.`login` FROM `messages` m LEFT JOIN `users` u ON `m`.`user_id`=`u`.`id`')->fetchAll();
+$messages = $mysql->query('SELECT m.`message`, m.`time`, u.`login` FROM `messages` m LEFT JOIN `users` u ON `m`.`user_id`=`u`.`id` ORDER BY m.`time` DESC')->fetchAll();
 
 $token = uniqid();
 echo template('index.html', [
