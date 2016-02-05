a3e6b522f5fad07a65cd6809cf9e0e7d129cf699
Anton V. Nazarov <a.nazarov@advmaker.net>
2016-02-05 13:11:21 +0300

    Оптимизация кода 6 урока  7й урок

--- lesson 7/src/index.php
+++ lesson 7/src/index.php
@@ -33,16 +33,31 @@ if (!empty($_GET['action'])) {
                 }
                 $query = $connection->prepare($sql);
                 $query->execute($params);
-                header('Location:http://epic-blog/lesson%206/src/index.php');
+                header('Location:http://epic-blog/lesson%207/src/index.php');
             }
             break;
     }
 }
 
-if (!empty($_POST['action']) && $_POST['action'] === 'save' && !empty($_POST['message'])) {
-    $query = $connection->prepare("INSERT INTO `messages` SET `message`=:message, `time`=NOW(), `user_id`=0");
-    $query->execute([':message' => $_POST['message']]);
-    header('Location:http://epic-blog/lesson%206/src/index.php');
+if (!empty($_POST['action'])) {
+    switch ($_POST['action']) {
+        case 'save':
+            if (!empty($_POST['message'])) {
+                $params = [
+                   ':message' => $_POST['message'],
+                ];
+                if (isset($_POST['message_id'])) {
+                    $params[':message_id'] = $_POST['message_id'];
+                    $sql = 'UPDATE `messages` SET `message`=:message, `user_id`=0 WHERE `id`=:message_id';
+                } else {
+                    $sql = 'INSERT INTO `messages` SET `message`=:message, `time`=NOW(), `user_id`=0';
+                }
+                $query = $connection->prepare($sql);
+                $query->execute($params);
+                header('Location:http://epic-blog/lesson%207/src/index.php');
+            }
+            break;
+    }
 }
 
 $messages =
@@ -50,6 +65,17 @@ $messages =
       ? $connection->query('SELECT m.`id`,m.`message`,m.`time`,u.`login` FROM `messages` m LEFT JOIN `users` u ON m.`user_id`=u.`id` ORDER BY m.`time` DESC')->fetchAll()
       : $connection->query("SELECT m.`id`,m.`message`,m.`time`,u.`login` FROM `messages` m LEFT JOIN `users` u ON m.`user_id`=u.`id` WHERE m.`id`={$message_id} ORDER BY m.`time` DESC")->fetchAll();
 
+$style = 'main';
+if (isset($_COOKIE['style'])) {
+    switch ((int)$_COOKIE['style']) {
+        case 1:
+            $style = 'black';
+            break;
+        case 2:
+            $style = 'yellow';
+            break;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -57,37 +83,14 @@ $messages =
 <head>
     <meta charset="UTF-8">
     <title>epic blog</title>
-    <style type="text/css">
-        body {
-            width: 60%;
-            margin-left: auto;
-            margin-right: auto;
-        }
-
-        .left {
-            float: left;
-        }
-
-        .right {
-            float: right;
-        }
-
-        .message {
-            margin-bottom: 20px;
-        }
-
-        textarea {
-            width: 100%;
-        }
-
-    </style>
+    <link rel="stylesheet" href="<?= $style ?>.css">
 </head>
 <body>
-<a href="http://epic-blog/lesson%206/src/index.php"><h1>Epic blog</h1></a>
+<a href="http://epic-blog/lesson%207/src/index.php"><h1>Epic blog</h1></a>
 <?php if (!empty($messages)): ?>
     <?php foreach ($messages as $message): ?>
         <div class="message">
-            <a href="http://epic-blog/lesson%206/src/index.php?action=show&message_id=<?= $message['id'] ?>"><h2>message № <?= $message['id'] ?></h2></a>
+            <a href="http://epic-blog/lesson%207/src/index.php?action=show&message_id=<?= $message['id'] ?>"><h2>message № <?= $message['id'] ?></h2></a>
 
             <div><?= htmlspecialchars($message['message']); ?></div>
             <span class="left"><?= $message['login']; ?></span>
@@ -96,8 +99,9 @@ $messages =
         <br/>
     <?php endforeach ?>
 <?php endif ?>
-<form action="http://epic-blog/lesson%206/src/index.php" method="post">
-    <textarea name="message" id="message" rows="10"></textarea>
+<form action="http://epic-blog/lesson%207/src/index.php" method="post">
+    <textarea name="message" id="message" rows="10"><?= empty($message_id) ? '' : $messages[0]['message'] ?></textarea>
+    <input type="hidden" name="message_id" value="<?= $message_id ?>">
     <input type="submit" name="action" value="save">
 </form>
 </body>
