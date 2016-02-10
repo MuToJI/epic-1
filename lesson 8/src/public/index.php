5a0b134781c8ebf7f8e4a74ca9b8dbf59097582a
puppollo <puppollo@gmail.com>
2016-02-10 20:20:14 +0300

    simple switch routing

--- lesson 8/src/public/index.php
+++ lesson 8/src/public/index.php
@@ -8,79 +8,76 @@ define('SITE_URL', 'http://epic-blog/lesson%208/src/public/index.php');
 require '../blog.php';
 
 $connection = connection(['host' => 'localhost', 'dbname' => 'blog', 'user' => 'root', 'password' => 'vagrant', 'encoding' => 'utf8']);
-$style = style($_COOKIE['style']);
+$style = style(empty($_COOKIE['style']) ? 0 : $_COOKIE['style']);
 $user = user();
 
-routes($_SERVER['REQUEST_URI'], [
-   'profile' => function ($params) use ($connection, $user, $style) {
-       if (empty($user)) {
-           header('Location:' . sprintf('%s?action=login', SITE_URL));
-       }
+$action = empty($_GET['action']) ? 'home' : $_GET['action'];
+switch ($action) {
+    case 'login':
+        if (!empty($user)) {
+            header('Location:' . sprintf('%s?action=home', SITE_URL));
+        }
+        $login = empty($_POST['login']) ? null : $_POST['login'];
+        $password = empty($_POST['password']) ? null : $_POST['password'];
 
-       if (isset($_POST['style'])) {
-           setcookie('style', $_POST['style'], 0, '/');
-           $style = style($_POST['style']);
-       }
+        if (!empty($_POST['token']) && valid_token($_POST['token'])) {
+            $user = user($connection, $login, $password);
+            if (!empty($user)) {
+                header('Location:' . sprintf('%s?action=home', SITE_URL));
+            }
+        }
 
-       echo template('../templates/profile.php', [
-          'site_url' => SITE_URL,
-          'style' => $style,
-       ]);
-   },
+        echo template('../templates/authorization.php', [
+           'token' => token(),
+           'login' => $login,
+           'site_url' => SITE_URL,
+           'style' => $style,
+        ]);
+        break;
+    case 'profile':
+        if (empty($user)) {
+            header('Location:' . sprintf('%s?action=login', SITE_URL));
+        }
 
-   'login' => function ($params) use ($connection, $user, $style) {
-       if (!empty($user)) {
-           header('Location:' . sprintf('%s?action=home', SITE_URL));
-       }
-       $login = empty($_POST['login']) ? null : $_POST['login'];
-       $password = empty($_POST['password']) ? null : $_POST['password'];
+        if (isset($_POST['style'])) {
+            setcookie('style', $_POST['style'], 0, '/');
+            $style = style($_POST['style']);
+        }
 
-       if (!empty($_POST['token']) && valid_token($_POST['token'])) {
-           $user = user($connection, $login, $password);
-           if (!empty($user)) {
-               header('Location:' . sprintf('%s?action=home', SITE_URL));
-           }
-       }
+        echo template('../templates/profile.php', [
+           'site_url' => SITE_URL,
+           'style' => $style,
+        ]);
+        break;
+    case 'save':
+        if (empty($user)) {
+            header('Location:' . sprintf('%s?action=login', SITE_URL));
+        }
 
-       echo template('../templates/authorization.php', [
-          'token' => token(),
-          'login' => $login,
-          'site_url' => SITE_URL,
-          'style' => $style,
-       ]);
-   },
+        $message_id = empty($_POST['message_id']) ? null : (int)$_POST['message_id'];
+        $message = empty($_POST['message']) ? null : $_POST['message'];
 
-   'save' => function ($params) use ($connection, $user) {
-       if (empty($user)) {
-           header('Location:' . sprintf('%s?action=login', SITE_URL));
-       }
+        if (!empty($message) && valid_token($_POST['token'])) {
+            isset($message_id)
+               ? update_message($connection, $message, $message_id)
+               : insert_message($connection, $message, $user['id']);
+        }
 
-       $message_id = empty($_POST['message_id']) ? null : (int)$_POST['message_id'];
-       $message = empty($_POST['message']) ? null : $_POST['message'];
+        header('Location:' . sprintf('%s?action=home&message_id=%d', SITE_URL, $message_id));
+        break;
+    default:
+        if (empty($user)) {
+            header('Location:' . sprintf('%s?action=login', SITE_URL));
+        }
 
-       if (!empty($message) && valid_token($_POST['token'])) {
-           isset($message_id)
-              ? update_message($connection, $message, $message_id)
-              : insert_message($connection, $message, $user['id']);
-       }
+        $message_id = empty($_GET['message_id']) ? null : (int)$_GET['message_id'];
+        $messages = load_messages($connection, $message_id);
 
-       header('Location:' . sprintf('%s?action=home&message_id=%d', SITE_URL, $message_id));
-   },
-
-   'home' => function ($params) use ($connection, $user, $style) {
-       if (empty($user)) {
-           header('Location:' . sprintf('%s?action=login', SITE_URL));
-       }
-
-       $message_id = empty($params['message_id']) ? null : (int)$params['message_id'];
-       $messages = load_messages($connection, $message_id);
-
-       echo template('../templates/home.php', [
-          'messages' => $messages,
-          'token' => token(),
-          'style' => $style,
-          'site_url' => SITE_URL,
-          'message_id' => $message_id,
-       ]);
-   }
-]);
+        echo template('../templates/home.php', [
+           'messages' => $messages,
+           'token' => token(),
+           'style' => $style,
+           'site_url' => SITE_URL,
+           'message_id' => $message_id,
+        ]);
+}
