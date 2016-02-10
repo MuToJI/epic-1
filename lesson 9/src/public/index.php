800f30cb0d72505e5ec188f2ff70c76862b0e562
Anton V. Nazarov <a.nazarov@advmaker.net>
2016-02-10 15:58:07 +0300

    fix index uses

--- lesson 9/src/public/index.php
+++ lesson 9/src/public/index.php
@@ -1,8 +1,6 @@
 <?php namespace Epic;
 
-use Epic\Controllers\Home;
-use Epic\Controllers\Login;
-use Epic\Controllers\Profile;
+use Epic\Controllers;
 
 session_start();
 ini_set('display_errors', 1);
@@ -15,7 +13,7 @@ require '../vendor/autoload.php';
 Lib\connection(['host' => 'localhost', 'dbname' => 'blog', 'user' => 'root', 'password' => 'vagrant', 'encoding' => 'utf8']);
 
 Lib\routes($_SERVER['REQUEST_URI'], [
-   'home' => new Home(),
-   'profile' => new Profile(),
-   'login' => new Login(),
+   'home' => new Controllers\Home(),
+   'profile' => new Controllers\Profile(),
+   'login' => new Controllers\Login(),
 ]);
