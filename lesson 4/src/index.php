16925844daa8203f83e2b1be463db42e02d9795b
puppollo <puppollo@gmail.com>
2016-05-14 15:56:59 +0300

    blog index.php

--- lesson 4/src/index.php
+++ lesson 4/src/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!empty($_GET['title']) && !empty($_GET['message'])) {
+    file_put_contents(__DIR__ . '/../messages/' . $_GET['title'], $_GET['message']);
+}
+
 $files = glob(__DIR__ . '/../messages/*');
 
 ?>
@@ -10,5 +14,12 @@ $files = glob(__DIR__ . '/../messages/*');
     <h1><?= basename($file) ?></h1>
     <div><?= nl2br(htmlentities(file_get_contents($file))) ?></div>
 <?php endforeach; ?>
+
+<form method="GET">
+    <input type="text" name="title"/><br>
+    <textarea name="message" id="" cols="30" rows="10"></textarea><br>
+    <input type="submit"/>
+</form>
+
 </body>
 </html>
